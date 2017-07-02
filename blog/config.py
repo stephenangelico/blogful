e8da21a66cb59e823ae99f209f8ec90381cce883
Stephen Angelico <raaff35lightning@gmail.com>
2017-07-03 00:04:56 +1000

    Add config for Travis CI

--- blog/config.py
+++ blog/config.py
@@ -8,3 +8,8 @@ class TestingConfig(object):
 	SQLALCHEMY_DATABASE_URI = "postgresql://stephen:thinkful@localhost:5432/blogful-test"
 	DEBUG = False
 	SECRET_KEY = "Not secret"
+
+class TravisConfig(object):
+	SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
+	DEBUG = False
+	SECRET_KEY = "Not secret"
