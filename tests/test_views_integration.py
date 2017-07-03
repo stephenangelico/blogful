41007613d69ec726f39b6f54c810ed0c1e500148
Stephen Angelico <raaff35lightning@gmail.com>
2017-07-03 10:37:21 +1000

    Fix filter and integration tests to work on Travis

--- tests/test_views_integration.py
+++ tests/test_views_integration.py
@@ -5,7 +5,8 @@ from urllib.parse import urlparse
 from werkzeug.security import generate_password_hash
 
 # Configure your app to use the testing database
-os.environ["CONFIG_PATH"] = "blog.config.TestingConfig"
+if os.environ.get("CONFIG_PATH") == None:
+	os.environ["CONFIG_PATH"] = "blog.config.TestingConfig"
 
 from blog import app
 from blog.database import Base, engine, session, User, Entry
